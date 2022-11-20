24af7e26d90fc5ea7fbc378a1bca65b08e07389f
Junior Duarte <junior.duarte@casion.com.br>
2022-11-20 00:30:36 -0300

    chore: ajustes

--- src/features/action/index.ts
+++ src/features/action/index.ts
@@ -1,7 +1,8 @@
 type GTMActionProps = { event: string; [key: string]: string };
 
 export const GTMAction = ({ event, ...more }: GTMActionProps) => {
-  if (typeof document !== 'undefined') return;
+  if (typeof window === 'undefined') return;
+
   window.dataLayer.push({
     event,
     ...more,
