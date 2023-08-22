3254c5eba66093758f4ebf11d299de396118e63f
jr-duarte <jr.duartee.93@gmail.com>
2023-08-22 11:16:31 -0300

    feat: alter scrips

--- src/features/init/index.ts
+++ src/features/init/index.ts
@@ -45,11 +45,16 @@ const GTMInit = (code: string) => {
   window.react_gtm_ts.init = true;
 
   const elScript = document.createElement('script');
-  const script = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${code}');`;
+  const script = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','${code}');`;
   elScript.innerHTML = script;
 
   const elNoscript = document.createElement('noscript');
-  const noscript = `<iframe src="https://www.googletagmanager.com/ns.html?id=${code}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
+  const noscript = `<iframe src="https://www.googletagmanager.com/ns.html?id=${code}"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
   elNoscript.innerHTML = noscript;
 
   document.head.insertBefore(elScript, document.head.childNodes[0]);
